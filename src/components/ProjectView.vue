fb2904693eab19667d27259de8d0671594f2f10c
H4ckerman666 <paletovicion@hotmail.com>
2023-02-24 01:52:01 -0600

    add carousel, home, and project views

---
+++ src/components/ProjectView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Carousel from "./Carousel.vue";
+
+const props = defineProps({
+  id: String,
+});
+console.log("🚀 ~ file: ProjectView.vue:5 ~ id:", props.id);
+</script>
+<template>
+  <Carousel />
+</template>
+<style scoped></style>
