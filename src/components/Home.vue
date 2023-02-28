492e39e9b3e70a4bcef119464f99f1790d725c42
H4ckerman666 <paletovicion@hotmail.com>
2023-02-27 18:59:00 -0600

    centralize project info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,47 +2,7 @@
 import AboutMe from "@/components/AboutMe.vue";
 import Footer from "@/components/Footer.vue";
 import ProjectsList from "@/components/ProjectsList.vue";
-
-const work = [
-  {
-    title: "Kazali",
-    image: "kazali.jpg",
-    type: "Front/Back development",
-    description: "Property Management Application",
-  },
-  {
-    title: "Rose",
-    image: "rose.png",
-    type: "Front/Back development",
-    description: "Picking and packing system",
-  },
-  {
-    title: "Flora",
-    image: "flora.png",
-    type: "Backend development",
-    description: "Slack Bot",
-  },
-];
-const myProjects = [
-  {
-    title: "Expenditure Control",
-    image: "expenditure-control.png",
-    type: "Front development",
-    description: "Expenditure Control Application",
-  },
-  {
-    title: "Crypto Currency Quote",
-    image: "crypto-currency-quote.png",
-    type: "Front development",
-    description: "Crypto Currency Quote",
-  },
-  {
-    title: "Veterinary Appointments",
-    image: "veterinary-appointments.png",
-    type: "Front development",
-    description: "Veterinary Appointments",
-  },
-];
+import { work, myProjects } from "@/data/";
 </script>
 <template>
   <AboutMe />
